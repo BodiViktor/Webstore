6f3986c9548c86f96059000fd1915e413d9c8f32
Bodi Viktor <bodiviktor1998@gmail.com>
2021-05-31 23:33:09 +0200

    innentől

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,20 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './shared/navbar/navbar.component';
-<<<<<<< HEAD
-import { ItemCardComponent } from './item-card/item-card.component';
-import { MatCardModule } from '@angular/material/card';
-
-=======
-
-import { ItemCardComponent } from './item-card/item-card.component';
-import { MatCardModule } from '@angular/material/card';
->>>>>>> 471b7aa69b9262b23adea3217f1758a341c023fd
 import { HomeComponent } from './pages/home/home.component';
+import { ItemCardComponent } from './item-card/item-card.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
